fb0b5d27bdab50ca15eefa4e16cbb45e27694e1a
cuileikun <583271702@qq.com>
2017-09-15 18:24:45 +0800

    第三种方式TabLayout+ViewPager+Fragment

--- app/src/main/java/mbcloud/com/jiemiandemo/MainActivity.java
+++ app/src/main/java/mbcloud/com/jiemiandemo/MainActivity.java
@@ -10,6 +10,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import mbcloud.com.jiemiandemo.first.FirstDemoActivity;
+import mbcloud.com.jiemiandemo.fouth.FouthDemoActivity;
 import mbcloud.com.jiemiandemo.second.SecondDemoActivity;
 import mbcloud.com.jiemiandemo.third.ThirdDemoActivity;
 
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button btn_second_method;
     @BindView(R.id.btn_third_method)
     Button btn_third_method;
+    @BindView(R.id.btn_fouth_method)
+    Button btn_fouth_method;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick({
-            R.id.btn_first_method, R.id.btn_second_method, R.id.btn_third_method})
+            R.id.btn_first_method, R.id.btn_second_method, R.id.btn_third_method,R.id.btn_fouth_method})
     void click(View view) {
         switch (view.getId()) {
             case R.id.btn_first_method:
@@ -40,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.btn_third_method:
                 startActivity(new Intent(MainActivity.this, ThirdDemoActivity.class));
-
+                break;
+            case R.id.btn_fouth_method:
+                startActivity(new Intent(MainActivity.this, FouthDemoActivity.class));
                 break;
         }
     }
